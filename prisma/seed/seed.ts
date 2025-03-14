08a879862f67891e64319f9e6e823b0e2e4789d4
macbook <macbook@MacBook-Air-ihajar.local>
2025-03-14 15:04:25 +0100

    2: Seeded the Database with a taxonomy data

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import { seedTaxonomy } from "./taxonomy.seed";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
+    await seedTaxonomy(prisma);
+}
+
+main().catch((e) => {
+    throw e;
+}).finally(async() => {
+    await prisma.$disconnect();
+})
