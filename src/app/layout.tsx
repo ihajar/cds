d8e9c2f72a6e86cd1d3d923ca9e0de46294db753
macbook <macbook@MacBook-Air-ihajar.local>
2025-03-14 18:27:02 +0100

    5: Building UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,31 @@
+import { Metadata } from "next";
+import { Mulish, Roboto } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+ 
+import { Toaster } from "@/components/ui/sonner";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
+
+export const metadata: Metadata = {
+  title: "Car Delings",
+  description: "an Ultimate platform for cars dealings",
+};
+
+const mulish = Mulish({
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-heading",
+  display: "swap",
+});
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-body",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -8,8 +34,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        {children}
+      <body
+        className={cn(
+          "antialiased overscroll-none bg-background",
+          roboto.variable,
+          mulish.variable
+        )}
+      >
+        <NextTopLoader showSpinner={false} />
+        <NuqsAdapter>{children}</NuqsAdapter>
+        <Toaster />
       </body>
     </html>
   );
